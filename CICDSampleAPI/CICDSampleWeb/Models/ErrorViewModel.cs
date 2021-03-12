d8067bc33ec0fd1b71e38028761c777f269f3ef5
Feryat Olcay <olcayyf@hotmail.com>
2021-03-12 12:53:35 +0000

    add erb site

---
+++ CICDSampleAPI/CICDSampleWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CICDSampleWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
